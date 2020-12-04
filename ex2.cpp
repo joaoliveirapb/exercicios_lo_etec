ae2e7386ca1f7f6771527928ec4cc44ef0508e37
joaoliveirapb <joaoliveira.batista1@gmail.com>
2020-12-04 19:25:29 -0300

    Ajuste no código

--- ex2.cpp
+++ ex2.cpp
@@ -4,20 +4,18 @@
 
 int main() {
 	setlocale(LC_ALL, "portuguese");
-	int diabete,com,pre,sem;
+	int diabete;
 	printf("Informe a porcentagem do seu exame de diabetes\n");
 	scanf("%i",&diabete);
 	
 	if(diabete>=126) {
 		printf("\nVocê está com diabetes, precisa se cuidar.\n");
 	}
-	else {
-		if(diabete<=99) {
+	else if(diabete<=99) {
 			printf("\nVocê não tem diabetes\n");
 		}
 		else{
 			printf("\nVocê está com pré-diabetes, tome cuidado!\n");
 		}
-	}
 	system("pause");
 }
